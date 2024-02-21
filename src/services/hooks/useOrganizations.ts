88ab10039e76d54c6ee269905108508c6e5ac421
Jenya Golovnov <93ave.blues@gmail.com>
2024-02-21 18:36:53 +0100

    removed console.log

--- src/services/hooks/useOrganizations.ts
+++ src/services/hooks/useOrganizations.ts
@@ -2,7 +2,6 @@ import useSWR from "swr";
 import { fetcher } from "../fetcher";
 
 export const useOrganizations = (query: string | null) => {
-    console.log('useOrganizations', query);
     return useSWR(
         query ? `https://api.github.com/search/users?q=${query}+type:org` : null,
         fetcher,
